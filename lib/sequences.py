d30bf0a8a4ae110558478fa6451b3d695509254a
maryam <Reemaali9048@gmail.com>
2023-11-23 18:51:56 +0300

    complete assigment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        fibonacci_sequence = []
+    elif length == 1:
+        fibonacci_sequence = [0]
+    else:
+        fibonacci_sequence = [0, 1]
+        for i in range(2, length):
+            next_element = fibonacci_sequence[i - 1] + fibonacci_sequence[i - 2]
+            fibonacci_sequence.append(next_element)
+    print(fibonacci_sequence)
